ad711bf8ce2544c14a0f8db7f2aca5ff202ecf68
Jin Lim <limjinsun@gmail.com>
2018-07-08 23:23:39 +0100

    LinkedList implementation.

--- LinkedList-C/main.cpp
+++ LinkedList-C/main.cpp
@@ -1,9 +1,83 @@
 // Copyright © Jin Lim. All rights reserved.
 
 #include <iostream>
+#include <assert.h>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+struct node {
+    int data;
+    node *next;
+};
+
+class LinkedList {
+    node *head;
+    node *tail;
+    
+public:
+    LinkedList(){
+        head = nullptr;
+        tail = nullptr;
+    };
+    
+    void createNode(int value);
+    void insertNodeAtStart(int value);
+    void insertNodeAtEnd(int value);
+    void display();
+};
+
+void LinkedList::createNode(int value){
+    assert (head == NULL && "List is not empty");
+    node *temp = new node;
+    temp->data = value;
+    temp->next = nullptr;
+    
+    head = temp;
+    tail = temp;
+    // delete temp;
+    temp = nullptr;
+};
+
+void LinkedList::insertNodeAtStart(int value){
+    node *temp = new node;
+    temp->data = value;
+    
+    temp->next = head;
+    head = temp;
+    // delete temp;
+    temp = nullptr;
+};
+
+void LinkedList::insertNodeAtEnd(int value){
+    node *temp = new node;
+    temp->data = value;
+    temp->next = nullptr;
+    
+    tail->next = temp;
+    tail = tail->next;
+    
+    // delete temp;
+    temp = nullptr;
+};
+
+void LinkedList::display(){
+    node *displayNode = new node;
+    displayNode = head;
+    while(displayNode != nullptr)
+    {
+        cout<<displayNode->data<<"\t";
+        displayNode=displayNode->next;
+    }
+};
+
+
+int main(){
+    LinkedList *list1 = new LinkedList();
+    list1->createNode(4);
+    list1->insertNodeAtEnd(5);
+    list1->insertNodeAtEnd(6);
+    list1->insertNodeAtEnd(7);
+    list1->insertNodeAtEnd(8);
+    list1->insertNodeAtStart(3);
+    
+    list1->display();
+};
